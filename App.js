79cc1031e10e636170f17ef4f4f09c98c88abbf3
anjali-codes91 <anjalichowdhury.skncoe.comp@gmail.com>
2024-07-09 23:22:55 +0530

    THANK YOU

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement(
+    "h1",{id:"heading"},"Heloo react!!");
+
+    // console.log(heading);
+
+            const root = ReactDOM.createRoot(document.getElementById("root"));
+
+            root.render(heading);
